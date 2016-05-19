c71dc3c4cee7fcca337b390b899196a565931255
Erki <erkihindo@gmail.com>
2016-05-19 10:41:37 +0300

    added message creation example

---
+++ build/generated/source/r/test/debug/com/github/ros_java/android_ROS/controller/InputManagerTest.java
@@ -0,0 +1,14 @@
+package com.github.ros_java.android_ROS.controller;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by viki on 5/18/16.
+ */
+public class InputManagerTest extends TestCase {
+
+    public void testLeftJoystickData() throws Exception {
+        InputManager IM = new InputManager();
+        assertTrue(true);
+    }
+}
